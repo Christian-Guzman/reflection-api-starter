312142fdbdb7071f7fff4d010db8ec42f696e037
Christian Guzman <christianguzman207@gmail.com>
2020-03-28 18:41:18 -0500

    Api completed

---
+++ src/main/java/org/basecampcodingacademy/reflections/OneReflectionPerDay.java
@@ -0,0 +1,10 @@
+package org.basecampcodingacademy.reflections;
+
+import java.time.LocalDate;
+
+public class OneReflectionPerDay extends Exception {
+    public LocalDate date;
+    public OneReflectionPerDay(LocalDate date) {
+        this.date = date;
+    }
+}
